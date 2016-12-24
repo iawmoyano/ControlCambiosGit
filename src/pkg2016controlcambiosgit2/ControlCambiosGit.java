26cd0615cdf3b68119a980b983174900e3be1d60
Trabajo <Trabajo@DESKTOP>
2016-12-24 16:07:13 +0100

    Cambio 2

--- src/pkg2016controlcambiosgit2/ControlCambiosGit.java
+++ src/pkg2016controlcambiosgit2/ControlCambiosGit.java
@@ -17,6 +17,7 @@ public class ControlCambiosGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Comienzo del proyecto");
+        System.out.println("Cambio 2");
     }
     
 }
