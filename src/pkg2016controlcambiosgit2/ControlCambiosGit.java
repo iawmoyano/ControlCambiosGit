032a4479f92a6087a16ab9c9dc01b7430a73a44e
Trabajo <Trabajo@DESKTOP>
2016-12-24 15:06:59 +0100

    Primera versión (David)

---
+++ src/pkg2016controlcambiosgit2/ControlCambiosGit.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg2016controlcambiosgit2;
+
+/**
+ *
+ * @author Trabajo
+ */
+public class ControlCambiosGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Comienzo del proyecto");
+    }
+    
+}
