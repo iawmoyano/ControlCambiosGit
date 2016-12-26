05885c7185f4193290559373efe1ce2164db4979
Trabajo <Trabajo@DESKTOP>
2016-12-26 17:28:24 +0100

    Cambio 33

--- src/pkg2016controlcambiosgit2/ControlCambiosGit.java
+++ src/pkg2016controlcambiosgit2/ControlCambiosGit.java
@@ -18,6 +18,7 @@ public class ControlCambiosGit {
         // TODO code application logic here
         System.out.println("Comienzo del proyecto");
         System.out.println("Cambio 22");
+        System.out.println("Cambio 33");
     }
     
 }
